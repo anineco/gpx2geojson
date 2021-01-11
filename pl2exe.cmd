096ab4e954884b456e242da0f0e4d0031de772ea
Anineco Nyanta <anineco@nifty.com>
2021-01-11 17:45:11 +0900

    Ver. 1.1

--- pl2exe.cmd
+++ pl2exe.cmd
@@ -1,5 +1,5 @@
 @echo off
-rem strawberry-perl-5.30.2.1-64bit
+rem strawberry-perl-5.30.3.1-64bit
 rem cpan Tk
 rem cpan PAR::Packer
-pp --gui -M XML::SAX::Expat -M XML::Parser::Expat -l libexpat-1__.dll -o gpx2geojson.exe gpx2geojson.pl
+pp --gui -M XML::LibXML::SAX -M XML::SAX::Expat -M XML::Parser::Expat -l libexpat-1__.dll -o gpx2geojson.exe gpx2geojson.pl
