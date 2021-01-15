3e9f6e58b95360a9c9c2d2fd95a978838b17a82c
Anineco Nyanta <anineco@nifty.com>
2021-01-16 02:44:54 +0900

    add depending dlls

--- pl2exe.cmd
+++ pl2exe.cmd
@@ -2,4 +2,5 @@
 rem strawberry-perl-5.30.3.1-64bit
 rem cpan Tk
 rem cpan PAR::Packer
-pp --gui -M XML::LibXML::SAX -M XML::SAX::Expat -M XML::Parser::Expat -l libexpat-1__.dll -o gpx2geojson.exe gpx2geojson.pl
+rem https://github.com/lucasg/Dependencies
+pp --gui -M XML::LibXML::SAX -l libxml2-2__.dll -l libiconv-2__.dll -l liblzma-5__.dll -l zlib1__.dll -o gpx2geojson.exe gpx2geojson.pl
